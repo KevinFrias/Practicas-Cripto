848c95bd0a423618c2fc8b425148d6346661325c
Kevin F <knightkevin99@gmail.com>
2023-05-05 15:26:58 -0600

    Encrypt image CBC good

--- Practica 3/CBC.py
+++ Practica 3/CBC.py
@@ -1,40 +1,25 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
-import os
+from PIL import Image
 
 
 def cifrar(llave, v0, archivo_entrada, archivo_salida):
-
     cipher = AES.new(llave, AES.MODE_CBC, v0)
-
-    with open(archivo_entrada, 'rb') as entrada, open(archivo_salida, 'wb') as salida:
-
-        while True :
-            bloque = entrada.read(16)
-
-            if not bloque :
-                break
-
-            bloque_pad = pad(bloque, 16)
-            salida.write(cipher.encrypt(bloque_pad))
+    imagen = Image.open(archivo_entrada)
+    imagen_bytes = imagen.tobytes()
+    imagen_cifrado = cipher.encrypt(pad(imagen_bytes, AES.block_size))
+    imagen_cifrado = Image.frombytes(imagen.mode, imagen.size, imagen_cifrado)
+    imagen_cifrado.save(archivo_salida)
 
 
-def descifrar(llave, v0, archivo_entrada, archivo_salida):
-
+def descifrar(llave, v0, archivo_entrada, archivo_salida):    
     cipher = AES.new(llave, AES.MODE_CBC, v0)
+    imagen_cifrado = Image.open(archivo_entrada)
+    imagen_bytes = imagen_cifrado.tobytes()
+    imagen_bytes = pad(imagen_bytes, AES.block_size)
 
-    with open(archivo_entrada, 'rb') as entrada, open(archivo_salida, 'wb') as salida:
+    imagen_bytes = cipher.decrypt(imagen_bytes)
+    
+    imagen_descifrado = Image.frombytes(imagen_cifrado.mode, imagen_cifrado.size, imagen_bytes)
 
-        while True :
-            bloque = entrada.read(32)
-
-            if not bloque :
-                break
-            
-            if len(bloque) % 16 != 0 :
-                bloque = pad(bloque, 16)
-
-            
-            bloque_unpad = unpad(cipher.decrypt(bloque), 16)
-
-            salida.write(bloque_unpad)
+    imagen_descifrado.save(archivo_salida)
